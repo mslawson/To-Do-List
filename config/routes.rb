3eebbb3a19ca9b9a654da9c1b37f3e68c1f3f08e
Matt Slawson <mdslawson@gmail.com>
2014-07-22 13:51:24 -0500

    Add length and presence validation to title

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Odot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'todo_lists#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "creating to do lists" do 
+	it "redirects to the todo lists index page on success" do
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My to-do list"
+		fill_in "Description", with: "This is what I do.  All day, every day."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My to-do list")
+	end
+
+	it "displays an error when the todo list doesn't have a title" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: "This is what I do.  All day, every day."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I do.")
+
+	end
+
+	it "displays an error when the todo list has a title less than 3 characters" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "This is what I do.  All day, every day."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I do.")
+
+	end
+end
