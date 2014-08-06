69436ae3f861c97bf6f796ca715ab17073d0174d
Matt Slawson <mdslawson@gmail.com>
2014-08-06 15:19:37 -0500

    Added validations to todo items.  Added CSS for alerts

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -19,8 +19,27 @@ describe "Adding todo items" do
 		within "ul.todo_items" do
 			expect(page).to have_content("Milk")
 		end
-		
 	end
 
+	it "displays an error with no content" do 
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within "div.flash" do
+			expect(page).to have_content("There was a problem with adding that todo.")
+		end
+		expect(page).to have_content("Content can't be blank")  
+	end
 
+	it "displays an error when item is not at least 3 characters" do 
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "ab"
+		click_button "Save"
+		within "div.flash" do
+			expect(page).to have_content("There was a problem with adding that todo.")
+		end
+		expect(page).to have_content("Content is too short ")  
+	end
 end
