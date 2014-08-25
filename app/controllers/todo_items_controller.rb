65b5f8762f777b1d3437aaf0c11bbeb8fcd51c29
Matt Slawson <mdslawson@gmail.com>
2014-08-25 18:22:33 -0500

    Cleaned up item views with form partial

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,13 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save
   		flash[:success] = "Added todo list item."
@@ -26,7 +24,7 @@ class TodoItemsController < ApplicationController
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
+    
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = "Saved todo list item."
@@ -43,6 +41,9 @@ class TodoItemsController < ApplicationController
   end
 
   private 
+  def find_todo_list 
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
   def todo_item_params
   	params[:todo_item].permit(:content)
   end
