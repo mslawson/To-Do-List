d8bb2589e9b50c7343cb615757448413a390df8b
Matt Slawson <mdslawson@gmail.com>
2014-08-28 16:42:46 -0500

    Cleaned up show view

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,4 +4,12 @@ class TodoList < ActiveRecord::Base
 	validates :title, :description, presence: true
 	validates :title, length: {minimum: 5}
 	validates :description, length: {minimum: 7}
+
+	def has_completed_items?  
+		todo_items.complete.size > 0 
+	end
+
+	def has_incompleted_items?
+		todo_items.incomplete.size > 0
+	end
 end
