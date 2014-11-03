809030f8197349c82f4304d93bb68977434cd488
Matt Slawson <mdslawson@gmail.com>
2014-11-03 11:34:22 -0600

    Updated tested for failed logins

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,5 +3,14 @@ class UserSessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to todo_lists_path
+  		flash[:success] = "Thanks for logging in!"
+  	else
+  		flash[:error] = "Sorry you must be a registered user."
+  		render action: 'new'
+  	end
   end
 end
