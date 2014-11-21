b2579c7be9e49161ca0bf06c6ca692ce61b05c06
Matt Slawson <mdslawson@gmail.com>
2014-11-21 11:01:39 -0600

    Added password reset token to users and created method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,7 +57,18 @@ describe User do
 			expect(user.save).to be_true
 			expect(user.email).to eq("mike@teamtreehouse.com") 
 		end
+	end
 
+	describe "#generate_password_reset_token!" do 
+		let(:user){create(:user)}
+		it "changes the password_reset_token_attribute" do
+			expect{user.generate_password_reset_token!}.to change{user.password_reset_token}
+		end 
+
+		it "calls SecureRandom.urlsafe_base64 to generate the password_reset_token" do
+			expect(SecureRandom).to receive(:urlsafe_base64)
+			user.generate_password_reset_token!
+		end	
 	end
 
 end
