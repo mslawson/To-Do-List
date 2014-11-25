762487c659a02758c8bf0fa85f3c4cc1458dce2d
Matt Slawson <mdslawson@gmail.com>
2014-11-25 14:11:34 -0600

    Fixed the password reset and implemented the mailer

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,20 +3,20 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:email])
-  	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.id
-  		redirect_to todo_lists_path
-  		flash[:success] = "Thanks for logging in!"
-  	else
-  		flash[:error] = "Please check your email and password"
-  		render action: 'new'
-  	end
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Thanks for logging in!"
+      redirect_to todo_lists_path
+    else
+      flash[:error] = "There was a problem logging in. Please check your email and password."
+      render action: 'new'
+    end
   end
 
   def destroy
     session[:user_id] = nil
-    reset_session
     redirect_to root_path, notice: "You have been logged out."
+    reset_session
   end
 end
